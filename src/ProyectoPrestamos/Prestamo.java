7bc1aa284be31311825847ecdd99e8901e83ad87
Juan <juan.lopmar.1@educa.jcyl.es>
2026-02-12 11:13:23 +0100

    Primer avance tercera clase

--- src/ProyectoPrestamos/Prestamo.java
+++ src/ProyectoPrestamos/Prestamo.java
@@ -1,6 +1,7 @@
 package ProyectoPrestamos;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 public class Prestamo {
     private String codigoLibro;
@@ -8,15 +9,36 @@ public class Prestamo {
     private Usuario socio;
     private LocalDate fechaPrestamo;
     private LocalDate fechaDevolucionPrevista;
-    private LocalDate fechaDevolucionReal;
+    private LocalDate fechaDevolucionReal=null;
 
 
     public Prestamo(String  codigoLibro,  Usuario socio, String tituloLibro, LocalDate fechaPrestamo) throws PrestamoInvalidoException {
+        this.socio = socio;
         if (codigoLibro.matches("[A-Z]{3}[0-9]{4}"))this.codigoLibro = codigoLibro;
             else throw new PrestamoInvalidoException("Código de libro erróneo");
-        if (!(tituloLibro.isEmpty()) || tituloLibro.matches(".+"))this.tituloLibro = tituloLibro;
+        if (!(tituloLibro.isEmpty()) && tituloLibro.matches(".+"))this.tituloLibro = tituloLibro;
             else throw new PrestamoInvalidoException("El libro debe contener un título");
-        if (fechaPrestamo!=null && (fechaPrestamo.isBefore(LocalDate.now())|| fechaPrestamo.isEqual(LocalDate.now())))this.fechaPrestamo = fechaPrestamo;
+        if (fechaPrestamo!=null && (fechaPrestamo.isBefore(LocalDate.now()) || fechaPrestamo.isEqual(LocalDate.now())))this.fechaPrestamo = fechaPrestamo;
             else throw new PrestamoInvalidoException("Fecha de préstamo inválida");
+        this.fechaDevolucionPrevista = fechaPrestamo.plusDays(14);
+    }
+
+
+    public void registrarDevolucion(LocalDate fechaDevolucion) throws PrestamoInvalidoException {
+        if  (fechaDevolucion!=null && (fechaDevolucion.isAfter(this.fechaPrestamo) || fechaDevolucion.isEqual(this.fechaPrestamo)))this.fechaDevolucionReal = fechaDevolucion;
+        else throw new PrestamoInvalidoException("Fecha de devolución inválida");
+    }
+
+    public int calcularDiasRetraso(){
+        int noRetraso;
+        if(this.fechaDevolucionReal==null) {
+            noRetraso = (int)ChronoUnit.DAYS.between(fechaDevolucionPrevista, LocalDate.now());
+            if (noRetraso<0){
+                return 0;
+            }
+            else return noRetraso;
+        }
+        else if ((ChronoUnit.DAYS.between(fechaDevolucionPrevista, this.fechaDevolucionReal))<0) return 0;
+        else return (int)(ChronoUnit.DAYS.between(fechaDevolucionPrevista, this.fechaDevolucionReal));
     }
 }
--- src/ProyectoPrestamos/PruebasClases.java
+++ src/ProyectoPrestamos/PruebasClases.java
@@ -6,7 +6,9 @@ public class PruebasClases {
     public static void main(String[] args) {
         try {
             Usuario u1 = new Usuario("pepe", "pa@b.lo", "SOC12345", LocalDate.now());
-            Prestamo p1 = new Prestamo("ABC1234",u1,"null",LocalDate.now().plusDays(0)); // seolucionar el null pointer exception cuando pasan null en un string para un constructor (catch en el main(buscar otra solución))
+            Prestamo p1 = new Prestamo("ABC1234",u1,"sef",LocalDate.now().plusDays(-17)); // solucionar el null pointer exception cuando pasan null en un string para un constructor(catch en el main)
+            p1.registrarDevolucion(LocalDate.now().plusDays(12));
+            System.out.println(p1.calcularDiasRetraso());
             u1.sancionar(1234455,LocalDate.now());
             System.out.println(u1.toString());
             u1.levantarSancion();
@@ -16,6 +18,9 @@ public class PruebasClases {
         }
         catch (PrestamoInvalidoException e) {
             System.out.println(e.getMessage());
+        }
+        catch (NullPointerException e){
+            System.out.println("Error inesperado");
         }
     }
 }
